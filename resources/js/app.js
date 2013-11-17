851bd4e368d139458ffe4f137c673c968ec52c82
matt <matt@matts-air.home>
2013-11-17 01:57:19 -0500

    Update to pjax transition css

--- resources/js/app.js
+++ resources/js/app.js
@@ -11,22 +11,32 @@ $.noConflict();
             }   
         },
 
+        pjaxBeforeSend = function() {
+            $('body').removeClass('loaded');
+        }
+
         pjaxCallback = function() {
             if($('body').hasClass('menu-open')) {
                 $('body').removeClass('menu-open');
             }
+            
+            $('body').addClass('loaded');
         };
 
         return {
             mobileMenuToggle: mobileMenuToggle,
+            pjaxBeforeSend: pjaxBeforeSend,
             pjaxCallback: pjaxCallback
         }
     }();
 
     $(document).pjax('.pjax-link', '#pjax-container');
+    $(document).on('pjax:beforeSend', appJS.main.pjaxBeforeSend);
     $(document).on('pjax:complete', appJS.main.pjaxCallback);
     
     $(document).ready(function() {
+        $('body').addClass('loaded');
+
         $('.menu-toggle').click(appJS.main.mobileMenuToggle);
     });
 })(jQuery);
